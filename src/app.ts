5dbe1455e93090fc2ae7cf7ff660a7a01e02316b
Vladimir <fayst_@bk.ru>
2021-10-03 14:18:40 +0300

    Настраивает соединение с удаленной базой MongoDB.

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,18 @@ import express, { Express } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import { urlencoded, json } from 'body-parser';
+import { connect } from 'mongoose';
 
 import { orderRouter } from './order/routes/order.route';
 import { categoryRoute } from './category/routes/category.route';
 import { authRouter } from './auth/routes/auth.toute';
 import { positionRoute } from './position/routes/position.route';
 import { analyticsRoute } from './analytics/routes/analytics.route';
+import {mongoEnvironment} from "./environments/mongo.environment";
+
+connect(`${mongoEnvironment.mongoURI}`)
+  .then(() => console.log('mongo connection'))
+  .catch((err: Error) => console.log('mongo connection error:', err));
 
 export const app: Express = express();
 app.use(morgan('dev'));
---
+++ src/environments/mongo.environment.ts
@@ -0,0 +1,5 @@
+export const mongoEnvironment = {
+    username: 'sv_user',
+    password: 'cEKqzMInsUXtkrIv',
+    mongoURI: `mongodb+srv://sv_user:cEKqzMInsUXtkrIv@cluster0.wuahj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+}
