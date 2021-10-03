f52bc693bd9aa7c2254e46826166e7113d35efb9
Vladimir <fayst_@bk.ru>
2021-10-03 22:22:58 +0300

    Реализует метод для авторизации по JWT.

--- src/auth/contollers/auth.controller.ts
+++ src/auth/contollers/auth.controller.ts
@@ -1,22 +1,50 @@
 import {Request, Response} from 'express';
-import { genSaltSync, hashSync } from 'bcryptjs';
+import { genSaltSync, hashSync, compareSync } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import { UserModel } from '../models/user.model';
 import { errorHandler } from '../../utils/errorHandler';
+import { UserInterface } from '../types/user.interface';
+import { jwtEnvironment } from '../../environments/jwt.environment';
 
 export class AuthController {
-  login = (req: Request, res: Response) => {
-    res.status(200).json({
-      login: {
-        email: req.body.email,
-        password: req.body.password
+  timeLifeToken = 60 * 60;
+  login = async (req: Request, res: Response): Promise<void> => {
+    // Проверяем есть ли введенный email в базе.
+    const candidate: UserInterface = await UserModel.findOne({ email: req.body.email });
+
+    if (!candidate) {
+      // Если такого email`a нет выбрасываем ошибку.
+      res.status(404).json({
+        errorMessage: 'Email не найден!'
+      });
+    } else {
+      // Проверяем пароль на совпадение.
+      const passwordResult = compareSync(req.body.password, candidate.password);
+      if (!passwordResult) {
+        // Если пароль не совпадает выдаем ошибку.
+        res.status(401).json({
+          errorMessage: 'Не верный пароль!'
+        });
+      } else {
+        // Если пароль совпадает генерируем токен.
+        const token = sign({
+          email: candidate.email,
+          _id: candidate._id
+        }, jwtEnvironment.SECRET_JWT, {
+          expiresIn: this.timeLifeToken
+        });
+        // Отправляем токен.
+        res.status(200).json({
+          token: `Token ${token}`
+        });
       }
-    });
+    }
   };
 
   register = async (req: Request, res: Response): Promise<void> => {
     // Проверяем есть ли введенный email в базе.
-    const candidate = await UserModel.findOne({ email: req.body.email });
+    const candidate: UserInterface = await UserModel.findOne({ email: req.body.email });
 
     if (candidate) {
       // Если есть выбрасываем ошибку
